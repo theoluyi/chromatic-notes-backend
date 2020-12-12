025596ff0a1a23b3c1d23488352b6a8f3d059027
github username <theoluyi@gmail.com>
2020-12-12 15:57:22 -0500

    adds users_controller boilerplate auth code

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,6 +1,9 @@
 class NotebooksController < ApplicationController
+    # we're adding an index action bc we want to be able to see all of 
+    # a user's notebooks in order to make the landing page clickable and responsive
     def index
         notebooks = Notebook.all
         render json: notebooks
     end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,55 @@
 class UsersController < ApplicationController
-    
-end
+    before_action :authorized, only: [:persist]
+  
+    def show
+      # YOUR PROJECTS SHOULD NOT INCLUDE A SHOW ACTION
+      @user = User.find(params[:id])
+      render json: @user
+    end
+  
+    def history
+      @user = User.find_by(username: params[:thisIsTheUserName])
+      if @user
+        render json: {count: @user.orders.count}
+      else
+        render json: {count: -2}
+      end
+    end
+  
+  
+    def persist
+      wristband = encode_token({user_id: @user.id})
+      render json: { user: UserSerializer.new(@user), token: wristband }
+    end
+  
+  
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        wristband = encode_token({user_id: @user.id})
+        render json: { user: UserSerializer.new(@user), token: wristband }, status: 201
+      else
+        render json: {message: "NICE TRY"}, status: 401
+      end
+    end
+  
+  
+    def login
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        wristband = encode_token({user_id: @user.id})
+        render json: { user: UserSerializer.new(@user), token: wristband }
+      else
+        render json: {message: "NICE TRY"}, status: 401
+      end
+    end
+  
+  
+    private
+  
+    def user_params
+      params.permit(:username, :password)
+    end
+  
+  end
+  
